b218c646a11415e13d6907ee9cf36a7ea3da719e
aliftareq <aliftareq@gmail.com>
2023-01-16 19:55:38 +0600

    company name and service title completed

--- src/Pages/Home/Brands.js
+++ src/Pages/Home/Brands.js
@@ -1,10 +1,38 @@
+import { Box, styled } from '@mui/material';
 import React from 'react';
+
+const brands = [
+    'https://i.ibb.co/27wHw8Y/image-5.png',
+    'https://i.ibb.co/vVSVd76/image-6.png',
+    'https://i.ibb.co/Dwj3KYc/image-7.png',
+    'https://i.ibb.co/99gcp3b/image-8.png',
+    'https://i.ibb.co/Rhk2VSh/image-9.png',
+];
+
+const BrandsWrapper = styled(Box)(({ theme }) => ({
+    marginBottom: '4rem',
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    '& > img': {
+        height: '60px'
+    },
+    [theme.breakpoints.down('md')]: {
+        flexDirection: 'column',
+        '& > img': {
+            marginBottom: '1rem',
+        },
+    },
+}))
 
 const Brands = () => {
     return (
-        <div>
-            <h1>this is brand page</h1>
-        </div>
+        <BrandsWrapper>
+            {
+                brands.map((brand, idx) => <img key={idx} src={brand} alt="" />)
+            }
+
+        </BrandsWrapper>
     );
 };
 
---
+++ src/Pages/Home/Services.js
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material';
+import React from 'react';
+import SectionTitle from '../../Component/SectionTitle/SectionTitle';
+
+const Services = () => {
+    return (
+        <Box>
+            <SectionTitle title={'Provide Awesome'} colored={'Services'} />
+        </Box>
+    );
+};
+
+export default Services;
