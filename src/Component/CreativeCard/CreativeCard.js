b4b785ea86f81109b4c2dd92ea2c9ce57ca04ded
aliftareq <aliftareq@gmail.com>
2023-01-18 11:07:01 +0600

    our works cards completed

--- src/Component/CreativeCard/CreativeCard.js
+++ src/Component/CreativeCard/CreativeCard.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Box, IconButton } from '@mui/material';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
-const CreativeCard = () => {
+const CreativeCard = ({ image }) => {
     return (
-        <div>
-
-        </div>
+        <Card sx={{ maxWidth: 345, minWidth: 345, p: 2, borderRadius: '10px' }}>
+            <CardMedia
+                sx={{ height: 180, borderRadius: '10px' }}
+                image={image}
+            />
+            <CardContent sx={{
+                p: 0,
+                pb: 0,
+                pt: 4,
+                '&:last-child': {
+                    pb: 0,
+                },
+                display: 'flex',
+                justifyContent: 'space-around',
+                alignItems: 'center',
+            }}>
+                <Box>
+                    <Typography gutterBottom variant="h5"
+                        component="div"
+                        color="text.secondary"
+                        fontSize='16px'
+                    >
+                        Web development
+                    </Typography>
+                    <Typography variant="body2"
+                        color='primary.main'
+                        fontWeight='bold'
+                        fontSize='20px'
+                    >
+                        Web design Research
+                    </Typography>
+                </Box>
+                <IconButton>
+                    <AddCircleOutlineIcon />
+                </IconButton>
+            </CardContent>
+        </Card>
     );
 };
 
--- src/Pages/Home/OurWorks.js
+++ src/Pages/Home/OurWorks.js
@@ -1,10 +1,12 @@
 
-import { Box, IconButton } from '@mui/material';
+import { Box, Grid, IconButton } from '@mui/material';
 import React, { useState } from 'react';
 import SectionTitle from '../../Component/SectionTitle/SectionTitle';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import CreativeCard from '../../Component/CreativeCard/CreativeCard';
+import { cardData } from './CardData';
 
 const OurWorks = () => {
     const [value, setValue] = useState(0);
@@ -16,71 +18,82 @@ const OurWorks = () => {
     return (
         <Box sx={{
             my: 5,
-            display: 'flex',
-            justifyContent: 'center',
         }}>
             {/* heading section  */}
             <Box sx={{
                 display: 'flex',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                gap: 2,
+                justifyContent: 'center',
             }}>
-                <SectionTitle
-                    title='Here are some of'
-                    colored='Our Works'
-                    sx={{
-                        textAlign: 'left',
-                    }}
-                />
                 <Box sx={{
-                    width: '100%',
-                    bgcolor: 'background.paper',
                     display: 'flex',
+                    justifyContent: 'space-between',
                     alignItems: 'center',
-
+                    gap: 2,
                 }}>
-                    <Tabs value={value} onChange={handleChange} centered sx={{
+                    <SectionTitle
+                        title='Here are some of'
+                        colored='Our Works'
+                        sx={{
+                            textAlign: 'left',
+                        }}
+                    />
+                    <Box sx={{
+                        width: '100%',
+                        bgcolor: 'background.paper',
+                        display: 'flex',
+                        alignItems: 'center',
 
-                        '& .MuiTabs-indicator': {
-                            display: 'none',
-                        },
-                        '& .Mui-selected': {
-                            color: 'Primary.lime',
-                            fontWeight: 'bold'
-                        },
-                        '& .MuiButtonBase-root': {
-                            textTransform: 'capitalize',
-                        }
                     }}>
-                        <Tab label="All" />
-                        <Tab label="Web Design" />
-                        <Tab label="Mobile-application" />
-                    </Tabs>
-                    <Box>
-                        <IconButton sx={{
-                            border: '1px solid #959EAD',
-                            mr: '1rem'
-                        }}
-                            onClick={() => setValue(value - 1)}
-                            disabled={value === 0}
-                        >
-                            <ArrowBackIcon />
-                        </IconButton>
-                        <IconButton sx={{
-                            border: '1px solid #959EAD'
-                        }}
-                            onClick={() => setValue(value + 1)}
-                            disabled={value === 2}
-                        >
-                            <ArrowBackIcon sx={{ transform: 'rotate(180deg)' }} />
-                        </IconButton>
+                        <Tabs value={value} onChange={handleChange} centered sx={{
+
+                            '& .MuiTabs-indicator': {
+                                display: 'none',
+                            },
+                            '& .Mui-selected': {
+                                color: 'Primary.lime',
+                                fontWeight: 'bold'
+                            },
+                            '& .MuiButtonBase-root': {
+                                textTransform: 'capitalize',
+                            }
+                        }}>
+                            <Tab label="All" />
+                            <Tab label="Web Design" />
+                            <Tab label="Mobile-application" />
+                        </Tabs>
+                        <Box>
+                            <IconButton sx={{
+                                border: '1px solid #959EAD',
+                                mr: '1rem'
+                            }}
+                                onClick={() => setValue(value - 1)}
+                                disabled={value === 0}
+                            >
+                                <ArrowBackIcon />
+                            </IconButton>
+                            <IconButton sx={{
+                                border: '1px solid #959EAD'
+                            }}
+                                onClick={() => setValue(value + 1)}
+                                disabled={value === 2}
+                            >
+                                <ArrowBackIcon sx={{ transform: 'rotate(180deg)' }} />
+                            </IconButton>
+                        </Box>
                     </Box>
                 </Box>
             </Box>
             {/* card section  */}
             <Box>
-
+                <Grid container spacing={3} justifyContent='center'>
+                    {
+                        cardData[value].map(image =>
+                            <Grid item>
+                                <CreativeCard image={image} />
+                            </Grid>
+                        )
+                    }
+                </Grid>
             </Box>
         </Box>
     );
